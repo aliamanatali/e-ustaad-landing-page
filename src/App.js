3ba9dd3843a36336dd6a35dc51353c0b2c198117
aliamanatali <aliamanatali218@gmail.com>
2023-12-03 16:56:25 +0500

    WIP Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Mainscreen from './components/mainscreen';
+import Navbar from './components/navbar';
+import Popularteachers from './components/popularteachers';
+import Topsubs from './components/topsubs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container screen">
+      <Navbar />
+      <Mainscreen />
+      <Popularteachers/>
+      <Topsubs/>
     </div>
   );
 }
---
+++ src/components/topsubs.js
@@ -0,0 +1,50 @@
+
+function topsubs() {
+
+  return (
+    <div className='container gap'>
+      <div className='text-center row mt-5' >
+        <h3 id='caladea'>Explore <span id='purple'>Top Subjects</span></h3>
+        <p>Click on the categories and explore all courses</p>
+      </div>
+      <div className='row'>
+        <div className='col-md-4 col-sm-12'>
+          <div className='card'>
+            <img src='https://images.unsplash.com/photo-1505238680356-667803448bb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29yZSUyMGJ1c2luZXNzJTIwY2l0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80' className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title'>Card title</h5>
+              <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href='#' className='btn btn-primary'>Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-4 col-sm-12'>
+          <div className='card'>
+            <img src='https://images.unsplash.com/photo-1505238680356-667803448bb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29yZSUyMGJ1c2luZXNzJTIwY2l0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80' className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title'>Card title</h5>
+              <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href='#' className='btn btn-primary'>Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-4 col-sm-12'>
+          <div className='card'>
+            <img src='https://images.unsplash.com/photo-1505238680356-667803448bb6?ixid=MnwxMjA3fDB8MH'
+              className='card-img-top' alt='...' />
+                          <div className='card-body'>
+              <h5 className='card-title'>Card title</h5>
+              <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href='#' className='btn btn-primary'>Go somewhere</a>
+            </div>
+           </div>
+           </div>
+           </div> 
+           
+    </div>
+
+
+  )
+}
+
+export default topsubs
